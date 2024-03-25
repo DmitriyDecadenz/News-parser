64087cb9f912fde1479fb5404fe2ba98ee894f26
DmitriyDecadenz <dima.hubul@icloud.com>
2024-03-25 14:00:51 +0400

    first refactor

--- main.py
+++ main.py
@@ -6,50 +6,59 @@ from pprint import pprint
 
 
 class GetData:
-    @staticmethod
-    def take_rss_data():
-        articles_json = []
-        r = requests.get('https://lenta.ru/rss/news')
-        soup = BeautifulSoup(r.content, features='lxml')
-        articles = soup.find_all('item')
-        for a in articles:
-            title = a.find('title').text
-            category = a.find('category').text
-            published = a.find('pubdate').text
-            article = {
-                'title': title,
-                'category': category,
-                'date': published
-                }
-            articles_json.append(article)
-        return articles_json
+    def __init__(self) -> None:
+        self.articles = []
+        self.url = 'https://lenta.ru/rss/news'
 
+    def take_rss_data(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.content, features="lxml")
+        self.articles = soup.find_all("item")
+        return self.articles
+
+
+class ParsData:
+    def __init__(self) -> None:
+        self.data = GetData()
+        self.data_list = []
+
+    def pars_data(self):
+        for a in self.data.take_rss_data():
+            title = a.find("title").text
+            category = a.find("category").text
+            published = a.find("pubdate").text
+            article = {"title": title, "category": category, "date": published}
+            self.data_list.append(article)
+        return self.data_list
 
 
 class Logging:
+
     @staticmethod
     def save_function(article_list) -> None:
-        with open('articles.json', 'w') as outfile:
+        with open("articles.json", "w") as outfile:
             for i in article_list:
-                json.dump(i, outfile,indent=2, ensure_ascii=False)
+                json.dump(i, outfile, indent=2, ensure_ascii=False)
+
 
 class Program:
+
     def __init__(self) -> None:
-        self.get_data = GetData()
+        self.get_data = ParsData()
         self.logger = Logging()
+
     def display(self) -> None:
-        data = self.get_data.take_rss_data()
+        data = self.get_data.pars_data()
         self.logger.save_function(data)
         for i in data:
             pprint(i)
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     while True:
         app = Program()
         app.display()
-        print('Scraping again in 5 minutes')
-        print('Finished scraping')
-        print('Saved to file')
+        print("Scraping again in 5 minutes")
+        print("Finished scraping")
+        print("Saved to file")
         time.sleep(5)
-      
